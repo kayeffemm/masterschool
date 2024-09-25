735e313afe86ef8081a5fa2adfe92ce612f1ae20
kayeffemm <kenneth.meier95@gmail.com>
2024-09-25 13:32:19 +0200

    created Built_in_functions.py

---
+++ Current Practice/Build_in_functions.py
@@ -0,0 +1,10 @@
+import random
+from math import sqrt
+"""for _ in range(10):
+    random_number_1_to_10 = random.randint(a = 1, b = 10)
+    print(random_number_1_to_10)"""
+
+def square_root(n):
+    return sqrt(n)
+
+print(square_root(81))
