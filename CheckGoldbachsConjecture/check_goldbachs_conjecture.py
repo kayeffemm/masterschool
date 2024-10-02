a285ac8f3c0dc1b18a3def1e9ca9ca0cde24bd52
kayeffemm <kenneth.meier95@gmail.com>
2024-10-02 11:32:09 +0200

    PEP8, better logic for is_prime()

--- CheckGoldbachsConjecture/check_goldbachs_conjecture.py
+++ CheckGoldbachsConjecture/check_goldbachs_conjecture.py
@@ -1,4 +1,9 @@
+# looks for all combinations of two primes that sum up to number.
 def get_prime_summands(number):
+    """
+    :param number: int
+    :return: list of tuples
+    """
     prime_summands = []
     for first_summand in range(2, int(number/2)): # avoid printing reversed duplicates
         if not is_prime(first_summand):
@@ -8,7 +13,11 @@ def get_prime_summands(number):
     return prime_summands
 
 
+# Exception handling -> check if user provided the requested input.
 def get_even_number_from_user():
+    """
+    :return: int
+    """
     while True:
         try:
             user_input_number = int(input("Please enter a even number: "))
@@ -19,14 +28,24 @@ def get_even_number_from_user():
             print("Thats not a number!")
 
 
+# checks if number is prime.
 def is_prime(number):
-    if number == 2:
-        return True
-    if number < 2 or number % 2 == 0:
+    """
+    :param number: int
+    :return: bool
+    """
+    if number <= 1:
         return False
-    for i in range(3, number, 2):
-        if number % i == 0:
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+
+    limit = int(number ** 0.5)
+    for i in range(5, limit + 1, 6):
+        if number % i == 0 or number % (i + 2) == 0:
             return False
+
     return True
 
 
